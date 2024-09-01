0d9b0370f1a23e964171b29862112748dab1ee65
Michael <mikova@microsoft.com>
2024-09-01 16:03:34 +0300

    removed provision identity functions

--- src/sempy_labs/_list_functions.py
+++ src/sempy_labs/_list_functions.py
@@ -2681,44 +2681,6 @@ def bind_dataset_to_gateway(
     )
 
 
-def provision_workspace_identity(workspace: Optional[str] = None):
-
-    # https://learn.microsoft.com/en-us/rest/api/fabric/core/workspaces/provision-identity?tabs=HTTP
-
-    workspace, workspace_id = resolve_workspace_name_and_id(workspace)
-
-    client = fabric.FabricRestClient()
-    response = client.post(f"/v1/workspaces/{workspace_id}/provisionIdentity")
-
-    if response.status_code not in [200, 202]:
-        raise FabricHTTPException(response)
-
-    lro(client, response)
-
-    print(
-        f"{icons.green_dot} A workspace identity has been provisioned for the '{workspace}' workspace."
-    )
-
-
-def deprovision_workspace_identity(workspace: Optional[str] = None):
-
-    # https://learn.microsoft.com/en-us/rest/api/fabric/core/workspaces/deprovision-identity?tabs=HTTP
-
-    workspace, workspace_id = resolve_workspace_name_and_id(workspace)
-
-    client = fabric.FabricRestClient()
-    response = client.post(f"/v1/workspaces/{workspace_id}/deprovisionIdentity")
-
-    if response.status_code not in [200, 202]:
-        raise FabricHTTPException(response)
-
-    lro(client, response)
-
-    print(
-        f"{icons.green_dot} The workspace identity has been deprovisioned from the '{workspace}' workspace."
-    )
-
-
 def list_deployment_pipelines() -> pd.DataFrame:
 
     df = pd.DataFrame(
