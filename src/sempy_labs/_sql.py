a0ff9a3d3e5fbbbede1c17f57c5d8e35c070da95
Michael <mikova@microsoft.com>
2024-09-07 22:08:38 +0300

    simplified

--- src/sempy_labs/_sql.py
+++ src/sempy_labs/_sql.py
@@ -6,7 +6,6 @@ import struct
 import uuid
 from itertools import chain, repeat
 from sempy.fabric.exceptions import FabricHTTPException
-import sempy_labs._icons as icons
 
 
 def bytes2mswin_bstr(value: bytes) -> bytes:
@@ -23,13 +22,8 @@ def bytes2mswin_bstr(value: bytes) -> bytes:
     :param value: the sequence of bytes to convert
     :return: the converted value (as a sequence of bytes)
     """
-    # The original code from the provided github URL applies 'Little-endian'
-    # to the byte conversion in a python for-loop.
-    # The following is code is assumed to be faster.
+
     encoded_bytes = bytes(chain.from_iterable(zip(value, repeat(0))))
-    # The original code applies native endianness for encoding the length
-    # prefix. My working assumption that also the length should be encoded
-    # 'Little-endian', regardless of the CPU (and OS) this code is running on.
     return struct.pack("<i", len(encoded_bytes)) + encoded_bytes
 
 
@@ -43,28 +37,21 @@ class ConnectWarehouse:
         from sempy.fabric._token_provider import SynapseTokenProvider
         import pyodbc
 
-        # get the TDS endpoint
-        client = fabric.FabricRestClient()
         workspace = fabric.resolve_workspace_name(workspace)
         workspace_id = fabric.resolve_workspace_id(workspace)
-        dfI = fabric.list_items(workspace=workspace, type="Warehouse")
+        warehouse_id = fabric.resolve_item_id(
+            item_name=warehouse, type="Warehouse", workspace=workspace
+        )
 
-        dfI_filt = dfI[dfI["Display Name"] == warehouse]
-        if len(dfI_filt) == 0:
-            raise ValueError(
-                f"{icons.red_dot} Invalid warehouse. The '{warehouse}' warehouse does not exist within the '{workspace}'  workspace."
-            )
-        warehouse_id = dfI_filt["Id"].iloc[0]
-
+        # get the TDS endpoint
+        client = fabric.FabricRestClient()
         response = client.get(f"v1/workspaces/{workspace_id}/warehouses/{warehouse_id}")
         if response.status_code != 200:
             raise FabricHTTPException(response)
-
         tds_endpoint = response.json().get("properties", {}).get("connectionString")
 
         access_token = SynapseTokenProvider()()
         tokenstruct = bytes2mswin_bstr(access_token.encode())
-
         conn_str = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={tds_endpoint};DATABASE={warehouse};Encrypt=Yes;"
 
         if timeout is not None:
