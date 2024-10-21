71fecb565b27b489a11de3972e857638261fbb8f
Michael <mikova@microsoft.com>
2024-10-21 12:32:44 +0200

    added 2 functions for getting dax query dependencies and related stats

--- src/sempy_labs/_dax.py
+++ src/sempy_labs/_dax.py
@@ -3,7 +3,9 @@ import pandas as pd
 from sempy_labs._helper_functions import (
     resolve_dataset_id,
     resolve_workspace_name_and_id,
+    format_dax_object_name,
 )
+from sempy_labs._model_dependencies import get_model_calc_dependencies
 from typing import Optional
 from sempy._utils._log import log
 
@@ -62,3 +64,104 @@ def evaluate_dax_impersonation(
     df = pd.DataFrame(data_rows, columns=column_names)
 
     return df
+
+
+@log
+def get_dax_query_dependencies(
+    dataset: str, dax_string: str, workspace: Optional[str] = None
+):
+
+    if workspace is None:
+        workspace = fabric.resolve_workspace_name(workspace)
+
+    # Escape quotes in dax
+    dax_string = dax_string.replace('"', '""')
+    final_query = f"""
+        EVALUATE
+        VAR source_query = "{dax_string}"
+        VAR all_dependencies = SELECTCOLUMNS(
+            INFO.CALCDEPENDENCY("QUERY", source_query), 
+                "Referenced Object Type",[REFERENCED_OBJECT_TYPE], 
+                "Referenced Table", [REFERENCED_TABLE],
+                "Referenced Object", [REFERENCED_OBJECT]
+            )             
+        RETURN all_dependencies
+        """
+    dep = fabric.evaluate_dax(
+        dataset=dataset, workspace=workspace, dax_string=final_query
+    )
+
+    # Clean up column names and values (remove outside square brackets, underscorees in object type)
+    dep.columns = dep.columns.map(lambda x: x[1:-1])
+    dep["Referenced Object Type"] = (
+        dep["Referenced Object Type"].str.replace("_", " ").str.title()
+    )
+    dep
+
+    # Dataframe df will contain the output of all dependencies of the objects used in the query
+    df = dep.copy()
+
+    cd = get_model_calc_dependencies(dataset=dataset, workspace=workspace)
+
+    for _, r in dep.iterrows():
+        ot = r["Referenced Object Type"]
+        object_name = r["Referenced Object"]
+        table_name = r["Referenced Table"]
+        cd_filt = cd[
+            (cd["Object Type"] == ot)
+            & (cd["Object Name"] == object_name)
+            & (cd["Table Name"] == table_name)
+        ]
+
+        # Adds in the dependencies of each object used in the query (i.e. relationship etc.)
+        if len(cd_filt) > 0:
+            subset = cd_filt[
+                ["Referenced Object Type", "Referenced Table", "Referenced Object"]
+            ]
+            df = pd.concat([df, subset], ignore_index=True)
+
+    df.columns = df.columns.map(lambda x: x.replace("Referenced ", ""))
+    # Remove duplicates
+    df = df.drop_duplicates().reset_index(drop=True)
+    # Only show columns and remove the rownumber column
+    df = df[
+        (df["Object Type"].isin(["Column", "Calc Column"]))
+        & (~df["Object"].str.startswith("RowNumber-"))
+    ]
+
+    # Get vertipaq stats, filter to just the objects in the df dataframe
+    df["Full Object"] = format_dax_object_name(df["Table"], df["Object"])
+    dfC = fabric.list_columns(dataset=dataset, workspace=workspace, extended=True)
+    dfC["Full Object"] = format_dax_object_name(
+        dfC["Table Name"], dfC["Column Name"]
+    )
+    dfC_filtered = dfC[dfC["Full Object"].isin(df["Full Object"].values)][
+        [
+            "Table Name",
+            "Column Name",
+            "Column Cardinality",
+            "Total Size",
+            "Data Size",
+            "Dictionary Size",
+            "Hierarchy Size",
+        ]
+    ].reset_index(drop=True)
+
+    return dfC_filtered
+
+
+@log
+def get_dax_query_memory_size(
+    dataset: str, dax_string: str, workspace: Optional[str] = None
+) -> int:
+
+    if workspace is None:
+        workspace = fabric.resolve_workspace_name(workspace)
+
+    df = get_dax_query_dependencies(
+        dataset=dataset,
+        workspace=workspace,
+        dax_string=dax_string,
+    )
+
+    return df["Total Size"].sum()
