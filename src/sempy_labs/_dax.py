701c9bf262cef3f1ae09b9031c3714eb75e804c2
Michael <mikova@microsoft.com>
2024-10-21 09:22:32 +0200

    added function for tracing dax queries

--- src/sempy_labs/_dax.py
+++ src/sempy_labs/_dax.py
@@ -1,11 +1,14 @@
 import sempy.fabric as fabric
 import pandas as pd
+from typing import Optional
+from sempy._utils._log import log
+import time
+import sempy_labs._icons as icons
 from sempy_labs._helper_functions import (
     resolve_dataset_id,
     resolve_workspace_name_and_id,
 )
-from typing import Optional
-from sempy._utils._log import log
+from sempy_labs._clear_cache import clear_cache
 
 
 @log
@@ -60,5 +63,60 @@ def evaluate_dax_impersonation(
     column_names = data[0]["rows"][0].keys()
     data_rows = [row.values() for item in data for row in item["rows"]]
     df = pd.DataFrame(data_rows, columns=column_names)
+
+    return df
+
+
+def analyze_dax(
+    dataset: str,
+    dax_queries: dict,
+    workspace: Optional[str] = None,
+    rest_time: int = 2,
+    clear_cache_before_run: bool = False,
+    clear_cache_before_each_query: bool = False,
+):
+
+    if workspace is None:
+        workspace = fabric.resolve_workspace_name()
+
+    base_cols = ["EventClass", "EventSubclass", "CurrentTime", "NTUserName", "TextData"]
+    begin_cols = base_cols + ["StartTime"]
+    end_cols = base_cols + ["StartTime", "EndTime", "Duration", "CpuTime", "Success"]
+
+    event_schema = {
+        "VertiPaqSEQueryBegin": begin_cols,
+        "VertiPaqSEQueryEnd": end_cols,
+        "VertiPaqSEQueryCacheMatch": base_cols,
+        "QueryBegin": begin_cols + ["ApplicationName"],
+        "QueryEnd": end_cols + ["ApplicationName"],
+    }
+
+    if clear_cache_before_run:
+        clear_cache(dataset=dataset, workspace=workspace)
+
+    # Establish trace connection
+    with fabric.create_trace_connection(dataset=dataset, workspace=workspace) as trace_connection:
+        with trace_connection.create_trace(event_schema) as trace:
+            trace.start()
+            # Loop through DAX queries
+            for i, (name, dax) in enumerate(dax_queries.items()):
+                # Clear cache for each query but not if done already before the run began
+                if clear_cache_before_each_query and not (i == 0 and clear_cache_before_run):
+                    clear_cache(dataset=dataset, workspace=workspace)
+
+                fabric.evaluate_dax(dataset=dataset, workspace=workspace, dax_string=dax)
+                time.sleep(rest_time)
+                print(f"{icons.green_dot} The '{name}' query has completed.")
+
+            df = trace.stop()
+            time.sleep(5)
+
+            # Name queries per dictionary
+            query_names = list(dax_queries.keys())
+            query_begin = df['Event Class'] == 'QueryBegin'
+            df['Query Name'] = (query_begin).cumsum()
+            df['Query Name'] = df['Query Name'].where(query_begin, None).ffill()
+            df['Query Name'] = pd.to_numeric(df['Query Name'], downcast='integer')
+            df['Query Name'] = df['Query Name'].map(lambda x: query_names[x - 1])
 
     return df
