0ccf45a75b09813135e3053cfd053334027401c5
Michael <mikova@microsoft.com>
2024-10-30 12:24:15 +0100

    added export_report

---
+++ src/sempy_labs/report/_export_report.py
@@ -0,0 +1,68 @@
+import sempy.fabric as fabric
+import sempy_labs._icons as icons
+from typing import Optional
+from sempy_labs._helper_functions import (
+    resolve_workspace_name_and_id,
+)
+from sempy_labs.lakehouse._lakehouse import lakehouse_attached
+from sempy.fabric.exceptions import FabricHTTPException
+
+
+def export_report(
+    report: str,
+    file_name: Optional[str] = None,
+    download_type: str = "LiveConnect",
+    workspace: Optional[str] = None,
+):
+    """
+    Exports the specified report from the specified workspace to a Power BI .pbix file.
+
+    This is a wrapper function for the following API: `Reports - Export Report In Group <https://learn.microsoft.com/rest/api/power-bi/reports/export-report-in-group>`.
+
+    Parameters
+    ----------
+    report: str
+        Name of the report.
+    file_name : str, default=None
+        Name of the .pbix file to be saved.
+        Defaults to None which resolves to the name of the report.
+    download_type : str, default="LiveConnect"
+        The type of download. Valid values are "LiveConnect" and "IncludeModel".
+    workspace : str, default=None
+        The Fabric workspace name.
+        Defaults to None which resolves to the workspace of the attached lakehouse
+        or if no lakehouse attached, resolves to the workspace of the notebook.
+    """
+
+    if not lakehouse_attached():
+        raise ValueError(
+            f"{icons.red_dot} A lakehouse must be attached to the notebook."
+        )
+
+    download_types = ["LiveConnect", "IncludeModel"]
+    if download_type not in download_types:
+        raise ValueError(
+            f"{icons.red_dot} Invalid download_type parameter. Valid options: {download_types}."
+        )
+
+    file_name = file_name or report
+    (workspace, workspace_id) = resolve_workspace_name_and_id(workspace)
+    report_id = fabric.resolve_item_id(
+        item_name=report, type="Report", workspace=workspace
+    )
+
+    client = fabric.PowerBIRestClient()
+    response = client.get(
+        f"/v1.0/myorg/groups/{workspace_id}/reports/{report_id}/Export?downloadType={download_type}"
+    )
+
+    if response.status_code != 200:
+        raise FabricHTTPException(response)
+
+    # Save file to the attached lakehouse
+    with open(f"/lakehouse/default/Files/{file_name}.pbix", "w") as file:
+        file.write(response.text)
+
+    print(
+        f"{icons.green_dot} The '{report}' report within the '{workspace}' workspace has been exported to the attached lakehouse as the '{file_name}' file."
+    )
