e8113924236812498d4fa2478d02c6171fce87c6
Edgar Cotte <edgar.cotte@microsoft.com>
2024-11-18 12:30:08 +0100

    Improved documentation for the Authentication Classes

--- src/sempy_labs/_authentication.py
+++ src/sempy_labs/_authentication.py
@@ -4,8 +4,45 @@ import notebookutils
 from azure.identity import ClientSecretCredential
 
 class ServicePrincipalTokenProviderWithKeyVault(TokenProvider):   
+    """
+    Implementation of the sempy.fabric.TokenProvider to be used with Service Principal providing the Azure Key Vault information.
 
-    def __init__(self, key_vault_uri, key_vault_tenant_id, key_vault_client_id, key_vault_client_secret):
+    For more information on Serice Principal check `Application and service principal objects in Microsoft Entra ID <https://learn.microsoft.com/en-us/entra/identity-platform/app-objects-and-service-principals?tabs=browser#service-principal-object>`_
+
+    For more information on Azure Key Vault check `About Azure Key Vault <https://learn.microsoft.com/en-us/azure/key-vault/general/overview>`_
+
+    Parameters
+    ----------
+    key_vault_uri : str 
+        Azure Key Vault URL.
+    key_vault_tenant_id:str,
+        Name of the secret in the Key Vault with the Fabric Tenant ID.
+    key_vault_client_id:str,
+        Name of the secret in the Key Vault with the SP Client ID.
+    key_vault_client_secret:str
+        Name of the secret in the Key Vault with the SP Client Secret.
+
+    """
+
+    def __init__(self, 
+        key_vault_uri:str, 
+        key_vault_tenant_id:str,
+        key_vault_client_id:str,
+        key_vault_client_secret:str
+        ):
+        """
+        Parameters
+        ----------
+        key_vault_uri : str 
+            Azure Key Vault URL.
+        key_vault_tenant_id:str,
+            Name of the secret in the Key Vault with the Fabric Tenant ID.
+        key_vault_client_id:str,
+            Name of the secret in the Key Vault with the SP Client ID.
+        key_vault_client_secret:str
+            Name of the secret in the Key Vault with the SP Client Secret.
+        """
+
         self.key_vault_uri = key_vault_uri
         self.key_vault_tenant_id = key_vault_tenant_id
         self.key_vault_client_id = key_vault_client_id
@@ -20,6 +57,12 @@ class ServicePrincipalTokenProviderWithKeyVault(TokenProvider):
         ) 
 
     def __call__(self, audience: Literal["pbi", "storage"] = "pbi") -> str:
+        """
+        Parameters
+        ----------
+        audience : Literal["pbi", "storage"] = "pbi") -> str
+            Literal if it's for PBI/Fabric API call or OneLake/Storage Account call.
+        """
         if audience == "pbi":
             return self.credential.get_token("https://analysis.windows.net/powerbi/api/.default").token
         elif audience == "storage":
@@ -28,9 +71,38 @@ class ServicePrincipalTokenProviderWithKeyVault(TokenProvider):
             raise NotImplementedError
 
 class ServicePrincipalTokenProvider(TokenProvider):   
+    """
+    Implementation of the sempy.fabric.TokenProvider to be used with Service Principal providing the SP properties.
 
-    def __init__(self, tenant_id, client_id, client_secret):
+    For more information on Serice Principal check `Application and service principal objects in Microsoft Entra ID <https://learn.microsoft.com/en-us/entra/identity-platform/app-objects-and-service-principals?tabs=browser#service-principal-object>`_
 
+    ***USE THIS ONE ONLY FOR TEST PURPOSE. FOR PRODUCTION WE RECOMEND THE ServicePrincipalTokenProviderWithKeyVault***
+
+    Parameters
+    ----------
+    tenant_id : str
+        Fabric Tenant ID.
+    client_id : str
+        Service Principal App Client ID.
+    client_secret : str
+        Service Principal Secret.
+    """
+
+    def __init__(self, 
+        tenant_id:str,
+        client_id:str,
+        client_secret:str
+        ):
+        """
+        Parameters
+        ----------
+        tenant_id : str
+            Fabric Tenant ID.
+        client_id : str
+            Service Principal App Client ID.
+        client_secret : str
+            Service Principal Secret.
+        """
         self.tenant_id = tenant_id
         self.client_id = client_id
         self.client_secret = client_secret
@@ -40,6 +112,12 @@ class ServicePrincipalTokenProvider(TokenProvider):
         ) 
 
     def __call__(self, audience: Literal["pbi", "storage"] = "pbi") -> str:
+        """
+        Parameters
+        ----------
+        audience : Literal["pbi", "storage"] = "pbi") -> str
+            Literal if it's for PBI/Fabric API call or OneLake/Storage Account call.
+        """
         if audience == "pbi":
             return self.credential.get_token("https://analysis.windows.net/powerbi/api/.default").token
         elif audience == "storage":
