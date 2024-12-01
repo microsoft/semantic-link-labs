dbe4a0c09bf484ce403257d2334842a55c761b00
Michael <mikova@microsoft.com>
2024-12-01 11:57:15 +0200

    fixed issue 314

--- src/sempy_labs/_model_bpa_bulk.py
+++ src/sempy_labs/_model_bpa_bulk.py
@@ -150,7 +150,7 @@ def run_model_bpa_bulk(
 
                         if df.empty:
                             df = bpa_df
-                        if not bpa_df.empty:
+                        elif not bpa_df.empty:
                             df = pd.concat([df, bpa_df], ignore_index=True)
                         print(
                             f"{icons.green_dot} Collected Model BPA stats for the '{dataset_name}' semantic model within the '{wksp}' workspace."
