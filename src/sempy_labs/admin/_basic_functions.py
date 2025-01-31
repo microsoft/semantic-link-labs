fb86f0ac415c6fb656ad02fac053c47ed476f15c
Nick Hurt <nick.hurt@gmail.com>
2025-01-31 09:54:04 +0000

    Fix issue #423
    Change response key for list tenant settings API

--- src/sempy_labs/admin/_basic_functions.py
+++ src/sempy_labs/admin/_basic_functions.py
@@ -329,7 +329,7 @@ def list_tenant_settings() -> pd.DataFrame:
 
     response = _base_api(request="/v1/admin/tenantsettings", client="fabric_sp")
 
-    for i in response.json().get('value', []):        
+    for i in response.json().get('value', []):
         new_data = {
             "Setting Name": i.get("settingName"),
             "Title": i.get("title"),
