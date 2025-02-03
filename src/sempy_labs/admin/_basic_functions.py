0e08b4e38f60ec49e623d39f2319ed34e53c2e76
Nick Hurt <nick.hurt@gmail.com>
2025-02-03 18:13:48 +0000

    Fix Issue 423 take 2
    Adding conditional logic in list_tenant_settings to cater for different API response keys

--- src/sempy_labs/admin/_basic_functions.py
+++ src/sempy_labs/admin/_basic_functions.py
@@ -329,7 +329,12 @@ def list_tenant_settings() -> pd.DataFrame:
 
     response = _base_api(request="/v1/admin/tenantsettings", client="fabric_sp")
 
-    for i in response.json().get("value", []):
+    if 'settingName' in response.json():
+        response_key = 'settingName'
+    else:
+        response_key = 'value'
+
+    for i in response.json().get(response_key, []):  
         new_data = {
             "Setting Name": i.get("settingName"),
             "Title": i.get("title"),
