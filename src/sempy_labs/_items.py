e335a624f5649a91a6f4d1dbc1b11e1d0d801c79
Michael <mikova@microsoft.com>
2025-04-03 11:05:36 -0700

    folders

--- src/sempy_labs/_items.py
+++ src/sempy_labs/_items.py
@@ -1,4 +1,6 @@
 import sempy.fabric as fabric
+import os
+import pandas as pd
 from sempy_labs._helper_functions import (
     _base_api,
     resolve_workspace_name_and_id,
@@ -9,6 +11,8 @@ from uuid import UUID
 from typing import Optional
 import json
 import sempy_labs._icons as icons
+from sempy_labs.lakehouse._blobs import list_blobs
+from sempy_labs._folders import list_folders
 
 
 def backup_item_definitions(
@@ -16,6 +20,9 @@ def backup_item_definitions(
     lakehouse: Optional[str | UUID] = None,
     lakehouse_workspace: Optional[str | UUID] = None,
 ):
+    """
+    Backups the item definitions of a workspace to a lakehouse.
+    """
 
     (workspace_name, workspace_id) = resolve_workspace_name_and_id(workspace)
     (lakehouse_workspace_name, lakehouse_workspace_id) = resolve_workspace_name_and_id(
@@ -25,6 +32,7 @@ def backup_item_definitions(
         lakehouse=lakehouse, workspace=lakehouse_workspace_id
     )
     local_path = _mount(lakehouse=lakehouse_id, workspace=lakehouse_workspace_id)
+    path_prefix = f"{local_path}/Files/{workspace_name}/"
 
     items = ["Report", "SemanticModel"]
 
@@ -35,7 +43,7 @@ def backup_item_definitions(
         item_name = r["Display Name"]
         item_id = r["Id"]
         description = r["Description"]
-        # folder_id = r['Folder Id']
+        folder_id = r["Folder Id"]
         item_type = r["Type"]
         definition = _base_api(
             request=f"/v1/workspaces/{workspace_id}/items/{item_id}/getDefinition",
@@ -44,10 +52,13 @@ def backup_item_definitions(
             status_codes=None,
         )
 
-        file_path = f"{local_path}/Files/{workspace_name}/{item_type}/{item_name}.json"
+        df_filt = dfF[dfF["Folder Id"] == folder_id]
+        folder_path = df_filt["Folder Path"].iloc[0]
 
-        definition['description'] = description
-        definition['folderId'] = None  # Update to folder_id
+        file_path = f"{path_prefix}/{item_type}/{item_name}.json"
+
+        definition["description"] = description
+        definition["folderPath"] = folder_path
 
         with open(file_path, "w") as json_file:
             json.dump(definition, json_file, indent=4)
@@ -56,6 +67,11 @@ def backup_item_definitions(
             f"{icons.green_dot} The '{item_name}' {item_type}' definition has been backed up to the Files section of the '{lakehouse_name}' lakehouse within the '{lakehouse_workspace_name}' workspace."
         )
 
+    # Save folder structure
+    dfF = list_folders(workspace=workspace)
+    with open(f"{path_prefix}/folderStructure.json", "w") as json_file:
+        json.dump(dfF.to_json(), json_file, indent=4)
+
 
 def restore_item_definitions(
     source_workspace: str,
@@ -63,8 +79,6 @@ def restore_item_definitions(
     lakehouse: Optional[str | UUID] = None,
     lakehouse_workspace: Optional[str | UUID] = None,
 ):
-
-    from sempy_labs.lakehouse._blobs import list_blobs
 
     (target_workspace_name, target_workspace_id) = resolve_workspace_name_and_id(
         target_workspace
@@ -86,21 +100,32 @@ def restore_item_definitions(
 
     local_path = _mount(lakehouse=lakehouse_id, workspace=lakehouse_workspace_id)
 
+    # Create the folder structure
+    with open(
+        f"{local_path}/Files/folderStructure.json", "r", encoding="utf-8"
+    ) as file:
+        df_folders = pd.json_normalize(json.load(file))
+        for _, r in df_folders.iterrows():
+            folder_name = r["Folder Name"]
+            folder_path = r["Folder Path"]
+
     for _, r in blobs_filt.iterrows():
         blob_name = r["Blob Name"]
         blob_file = blob_name.split(f"{lakehouse_id}")[1][1:]
-        split = blob_name.split("/")
-        item_type = split[-2]
-        item_name = split[-1].replace(".json", "")
+        file_name = os.path.basename(blob_file)
+        # directory = os.path.dirname(blob_file)
+        # folder_structure = os.path.dirname(directory)
+        item_type = os.path.basename(os.path.dirname(blob_file))
+        item_name = os.path.splitext(file_name)[0]
         definition_file_path = f"{local_path}/{blob_file}"
         with open(definition_file_path, "r", encoding="utf-8") as file:
             definition = json.load(file)
 
-        description = definition.get('definition')
-        folder_id = definition.get('folderId')
+        description = definition.get("definition")
+        folder_path = definition.get("folderPath")
 
-        definition.pop('description')
-        definition.pop('folderId')
+        definition.pop("description")
+        definition.pop("folderPath")
 
         payload = {
             "displayName": item_name,
@@ -117,4 +142,3 @@ def restore_item_definitions(
             status_codes=[201, 202],
             lro_return_status_code=True,
         )
-    
