212a4f71457814c7af5e33990814b9c201dae61c
Michael <mikova@microsoft.com>
2025-05-05 13:51:48 +0300

    solved with Darren

--- src/sempy_labs/_vpax.py
+++ src/sempy_labs/_vpax.py
@@ -2,7 +2,6 @@ import os
 import sempy
 import re
 from urllib.parse import urlparse
-import json
 import sempy.fabric as fabric
 import sys
 from pathlib import Path
@@ -12,15 +11,12 @@ from sempy_labs._helper_functions import (
     resolve_workspace_name_and_id,
     resolve_dataset_name_and_id,
     resolve_lakehouse_name_and_id,
-    resolve_workspace_id,
     _mount,
     _get_column_aggregate,
     resolve_item_type,
 )
 import sempy_labs._icons as icons
 from sempy_labs.lakehouse._blobs import list_blobs
-import zipfile
-import io
 from sempy_labs.tom import connect_semantic_model
 
 _vpa_initialized = False
@@ -113,7 +109,7 @@ def create_vpax(
 
     direct_lake_mode = direct_lake_mode.capitalize()
 
-    workspace_id = resolve_workspace_id(workspace)
+    (workspace_name, workspace_id) = resolve_workspace_name_and_id(workspace)
     (dataset_name, dataset_id) = resolve_dataset_name_and_id(dataset, workspace_id)
     (lakehouse_workspace_name, lakehouse_workspace_id) = resolve_workspace_name_and_id(
         lakehouse_workspace
@@ -122,7 +118,7 @@ def create_vpax(
         lakehouse=lakehouse, workspace=lakehouse_workspace
     )
 
-    local_path = _mount(lakehouse=lakehouse, workspace=lakehouse_workspace)
+    local_path = _mount(lakehouse=lakehouse_id, workspace=lakehouse_workspace_id)
     if file_path is None:
         file_path = dataset_name
     path = f"{local_path}/Files/{file_path}.vpax"
@@ -141,20 +137,9 @@ def create_vpax(
     extractor_app_version = "1.0"
     column_batch_size = 50
     token = notebookutils.credentials.getToken("pbi")
-    connection_string = f"data source=powerbi://api.powerbi.com/v1.0/myorg/{workspace};initial catalog={dataset_name};User ID=;Password={token};Persist Security Info=True;Impersonation Level=Impersonate "
+    connection_string = f"data source=powerbi://api.powerbi.com/v1.0/myorg/{workspace_name};initial catalog={dataset_name};User ID=;Password={token};Persist Security Info=True;Impersonation Level=Impersonate "
 
     print(f"{icons.in_progress} Extracting .vpax metadata...")
-
-    # dl_mode = (
-    #    DirectLakeExtractionMode.Full
-    #    if direct_lake_mode == "Full"
-    #    else DirectLakeExtractionMode.ResidentOnly
-    # )
-
-    # Do not use TOMExtractor to get stats for Direct Lake
-    with connect_semantic_model(dataset=dataset, workspace=workspace) as tom:
-        is_direct_lake = tom.is_direct_lake()
-    #    read_stats_using_tom_extractor = not is_direct_lake and read_stats_from_data
 
     # Get stats for the model; for direct lake only get is_resident
     dax_model = TomExtractor.GetDaxModel(
@@ -170,29 +155,18 @@ def create_vpax(
     vpa_model = Model(dax_model)
     tom_database = TomExtractor.GetDatabase(connection_string)
 
-    VpaxTools.ExportVpax(vpax_stream, dax_model, vpa_model, tom_database)
-
     # Calculate Direct Lake stats for columns which are IsResident=False
-    if read_stats_from_data and is_direct_lake and direct_lake_mode == "Full":
-        from System import Array, Byte
+    with connect_semantic_model(dataset=dataset, workspace=workspace) as tom:
+        is_direct_lake = tom.is_direct_lake()
+        if read_stats_from_data and is_direct_lake and direct_lake_mode == "Full":
+            from System import Array, Byte
 
-        # Extract vertipaq json from the vpax stream
-        vpax_stream.Position = 0
-        buffer = Array.CreateInstance(Byte, vpax_stream.Length)
-        vpax_stream.Read(buffer, 0, vpax_stream.Length)
-        vpax_bytes = bytes(bytearray(buffer))
-        vpax_zip = zipfile.ZipFile(io.BytesIO(vpax_bytes), mode="r")
-        vpa_json = vpax_zip.read("DaxVpaView.json").decode("utf-8")
-        cleaned_data = vpa_json.lstrip("\ufeff")
-        data_dict = json.loads(cleaned_data)
+            df_not_resident = fabric.evaluate_dax(
+                dataset=dataset,
+                workspace=workspace,
+                dax_string=""" SELECT [DIMENSION_NAME] AS [TableName], [ATTRIBUTE_NAME] AS [ColumnName] FROM $SYSTEM.DISCOVER_STORAGE_TABLE_COLUMNS WHERE NOT [ISROWNUMBER] AND NOT [DICTIONARY_ISRESIDENT]""",
+            )
 
-        df_not_resident = fabric.evaluate_dax(
-            dataset=dataset,
-            workspace=workspace,
-            dax_string=""" SELECT [DIMENSION_NAME] AS [TableName], [ATTRIBUTE_NAME] AS [ColumnName] FROM $SYSTEM.DISCOVER_STORAGE_TABLE_COLUMNS WHERE NOT [ISROWNUMBER] AND NOT [DICTIONARY_ISRESIDENT]""",
-        )
-
-        with connect_semantic_model(dataset=dataset, workspace=workspace) as tom:
             import Microsoft.AnalysisServices.Tabular as TOM
 
             print(f"{icons.in_progress} Calculating Direct Lake statistics...")
@@ -271,7 +245,7 @@ def create_vpax(
                                 workspace=item_workspace_id,
                                 table_name=entity_name,
                                 schema_name=schema_name,
-                                column_name=list(col_dict.keys()),
+                                column_name=list(col_dict.values()),
                                 function="distinctcount",
                             )
                             column_cardinalities = {
@@ -279,82 +253,18 @@ def create_vpax(
                                 for column_name, source_column in col_dict.items()
                                 if source_column in col_agg
                             }
-                # else:
-                #    columns = [
-                #        c.Name for c in t.Columns if c.Type != TOM.ColumnType.RowNumber
-                #    ]
-                #    dax = _dax_distinctcount(table_name, columns)
-                #    dax_result = fabric.evaluate_dax(
-                #        dataset=dataset, workspace=workspace, dax_string=dax
-                #    )
-                #    column_cardinalities = dax_result.iloc[0].to_dict()
-                # Update the vpax file with column cardinalities
-                for item_type, items in data_dict.items():
-                    if item_type == "Columns":
-                        for i, c in enumerate(items):  # get index and item
-                            t_name = c.get("TableName")
-                            c_name = c.get("ColumnName")
-                            if t_name == t.Name and c.get("ColumnType") != "RowNumber":
-                                c["ColumnCardinality"] = column_cardinalities.get(
-                                    c_name
-                                )
-                                items[i] = _move_key_to_index(
-                                    c, "ColumnCardinality", 3
-                                )  # update in-place
 
-        # Update the vpax file with relationship cardinalities
-        for item_type, items in data_dict.items():
-            if item_type == "Relationships":
-                for i, r in enumerate(items):
-                    from_column_full = r.get("FromFullColumnName")
-                    to_column_full = r.get("ToFullColumnName")
+                # Update the dax_model file with column cardinalities
+                for tbl in dax_model.Tables:
+                    t_name = tbl.TableName
+                    for col in tbl.Columns:
+                        c_name = col.ColumnName
+                        if str(col.ColumnType) != "RowNumber" and str(t_name) == t.Name:
+                            col.ColumnCardinality = column_cardinalities.get(
+                                str(c_name)
+                            )
 
-                    from_cardinality = next(
-                        c.get("ColumnCardinality")
-                        for c in data_dict["Columns"]
-                        if f"'{c.get('TableName')}'[{c.get('ColumnName')}]"
-                        == from_column_full
-                    )
-
-                    to_cardinality = next(
-                        c.get("ColumnCardinality")
-                        for c in data_dict["Columns"]
-                        if f"'{c.get('TableName')}'[{c.get('ColumnName')}]"
-                        == to_column_full
-                    )
-
-                    r["FromCardinality"] = from_cardinality
-                    r["ToCardinality"] = to_cardinality
-
-                    # Update the item in-place
-                    r = _move_key_to_index(r, "FromCardinality", 2)
-                    r = _move_key_to_index(r, "ToCardinality", 6)
-                    items[i] = r  # <--- this line ensures the data_dict is updated
-                    r["UsedSizeFrom"] = 0
-                    r["UsedSize"] = 0
-                    r["OneToManyRatio"] = (
-                        from_cardinality / to_cardinality if to_cardinality else 0
-                    )
-
-        # Step 1: Convert data_dict to JSON string and encode it
-        updated_json_str = json.dumps(data_dict, indent=2)
-        updated_json_bytes = updated_json_str.encode("utf-8")
-
-        # Step 2: Recreate the vpax ZIP archive in memory, replacing "DaxVpaView.json"
-        new_vpax_stream = io.BytesIO()
-        with zipfile.ZipFile(
-            new_vpax_stream, mode="w", compression=zipfile.ZIP_DEFLATED
-        ) as new_zip:
-            # Copy original contents except DaxVpaView.json
-            for item in vpax_zip.infolist():
-                if item.filename != "DaxVpaView.json":
-                    new_zip.writestr(item, vpax_zip.read(item.filename))
-            # Add updated DaxVpaView.json
-            new_zip.writestr("DaxVpaView.json", updated_json_bytes)
-
-        # Step 3: Reset the stream position
-        new_vpax_stream.seek(0)
-        vpax_stream = MemoryStream(new_vpax_stream.getvalue())
+    VpaxTools.ExportVpax(vpax_stream, dax_model, vpa_model, tom_database)
 
     print(f"{icons.in_progress} Exporting .vpax file...")
 
@@ -367,7 +277,7 @@ def create_vpax(
         f"{icons.green_dot} The {file_path}.vpax file has been saved in the '{lakehouse_name}' lakehouse within the '{lakehouse_workspace_name}' workspace."
     )
 
-    return vpax_stream
+    # return (vpax_stream, dax_model, vpa_model, tom_database)
 
 
 def _dax_distinctcount(table_name, columns):
