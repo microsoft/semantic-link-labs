8061268d04c8794f22d2882e51c7e9849405650e
Lulzim Bilali <lulzimbilali@gmail.com>
2025-05-12 12:47:54 +0200

    Add format parameter when receiving/creating or updating notebook definitions (#560)
    * add format parameter to notebook definitions
    
    * code simplification as pointed out during code review
    
    ---------
    
    Co-authored-by: Lulzim Bilali <lulzim.bilali@akersolutions.com>

--- src/sempy_labs/_notebooks.py
+++ src/sempy_labs/_notebooks.py
@@ -20,13 +20,20 @@ _notebook_prefix = "notebook-content."
 
 
 def _get_notebook_definition_base(
-    notebook_name: str, workspace: Optional[str | UUID] = None
+    notebook_name: str,
+    workspace: Optional[str | UUID] = None,
+    format: Optional[str] = None
 ) -> pd.DataFrame:
 
     (workspace_name, workspace_id) = resolve_workspace_name_and_id(workspace)
     item_id = resolve_item_id(item=notebook_name, type="Notebook", workspace=workspace)
+
+    url = f"v1/workspaces/{workspace_id}/notebooks/{item_id}/getDefinition"
+    if format == 'ipynb':
+        url += f"?format={format}"
+   
     result = _base_api(
-        request=f"v1/workspaces/{workspace_id}/notebooks/{item_id}/getDefinition",
+        request=url,
         method="post",
         lro_return_json=True,
         status_codes=None,
@@ -53,7 +60,10 @@ def _get_notebook_type(
 
 
 def get_notebook_definition(
-    notebook_name: str, workspace: Optional[str | UUID] = None, decode: bool = True
+    notebook_name: str,
+    workspace: Optional[str | UUID] = None,
+    decode: bool = True,
+    format: Optional[str] = None
 ) -> str:
     """
     Obtains the notebook definition.
@@ -71,6 +81,9 @@ def get_notebook_definition(
     decode : bool, default=True
         If True, decodes the notebook definition file into .ipynb format.
         If False, obtains the notebook definition file in base64 format.
+    format : str, default=None
+        The only supported value is ipynb
+        If provided the format will be in standard .ipynb otherwise the format will be in source code format which is GIT friendly ipynb
 
     Returns
     -------
@@ -79,7 +92,7 @@ def get_notebook_definition(
     """
 
     df_items = _get_notebook_definition_base(
-        notebook_name=notebook_name, workspace=workspace
+        notebook_name=notebook_name, workspace=workspace, format=format
     )
     df_items_filt = df_items[df_items["path"].str.startswith(_notebook_prefix)]
     payload = df_items_filt["payload"].iloc[0]
@@ -163,6 +176,7 @@ def create_notebook(
     type: str = "py",
     description: Optional[str] = None,
     workspace: Optional[str | UUID] = None,
+    format: Optional[str] = None
 ):
     """
     Creates a new notebook with a definition within a workspace.
@@ -182,19 +196,24 @@ def create_notebook(
         The name or ID of the workspace.
         Defaults to None which resolves to the workspace of the attached lakehouse
         or if no lakehouse attached, resolves to the workspace of the notebook.
+    format : str, default=None
+        If 'ipynb' is provided than notebook_content should be standard ipynb format
+        otherwise notebook_content should be GIT friendly format
     """
 
-    notebook_payload = base64.b64encode(notebook_content).decode("utf-8")
+    notebook_payload = base64.b64encode(notebook_content.encode("utf-8")).decode("utf-8")
     definition_payload = {
-        "format": "ipynb",
         "parts": [
             {
-                "path": f"{_notebook_prefix}.{type}",
+                "path": f"{_notebook_prefix}{type}",
                 "payload": notebook_payload,
                 "payloadType": "InlineBase64",
             }
         ],
     }
+
+    if format == 'ipynb':
+        definition_payload["format"] = "ipynb"
 
     create_item(
         name=name,
@@ -206,7 +225,10 @@ def create_notebook(
 
 
 def update_notebook_definition(
-    name: str, notebook_content: str, workspace: Optional[str | UUID] = None
+    name: str,
+    notebook_content: str,
+    workspace: Optional[str | UUID] = None,
+    format: Optional[str] = None
 ):
     """
     Updates an existing notebook with a new definition.
@@ -221,10 +243,13 @@ def update_notebook_definition(
         The name or ID of the workspace.
         Defaults to None which resolves to the workspace of the attached lakehouse
         or if no lakehouse attached, resolves to the workspace of the notebook.
+    format : str, default=None
+        If 'ipynb' is provided than notebook_content should be standard ipynb format
+        otherwise notebook_content should be GIT friendly format
     """
 
     (workspace_name, workspace_id) = resolve_workspace_name_and_id(workspace)
-    notebook_payload = base64.b64encode(notebook_content)
+    notebook_payload = base64.b64encode(notebook_content.encode("utf-8")).decode("utf-8")
     item_id = resolve_item_id(item=name, type="Notebook", workspace=workspace)
     type = _get_notebook_type(notebook_name=name, workspace=workspace)
 
@@ -232,13 +257,16 @@ def update_notebook_definition(
         "definition": {
             "parts": [
                 {
-                    "path": f"{_notebook_prefix}.{type}",
+                    "path": f"{_notebook_prefix}{type}",
                     "payload": notebook_payload,
                     "payloadType": "InlineBase64",
                 }
             ],
         },
     }
+
+    if format == 'ipynb':
+        payload["definition"]["format"] = "ipynb"
 
     _base_api(
         request=f"v1/workspaces/{workspace_id}/notebooks/{item_id}/updateDefinition",
