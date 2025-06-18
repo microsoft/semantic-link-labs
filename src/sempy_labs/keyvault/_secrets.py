bd23052eebe1aa6e7460963df09eecb10d39cac6
Michael <mikova@microsoft.com>
2025-06-18 22:59:49 +0300

    fix secrets

--- src/sempy_labs/keyvault/_secrets.py
+++ src/sempy_labs/keyvault/_secrets.py
@@ -2,6 +2,7 @@ from typing import Any
 import pandas as pd
 from sempy_labs._helper_functions import (
     _base_api,
+    _create_dataframe,
 )
 import sempy_labs._icons as icons
 from sempy._utils._log import log
@@ -69,7 +70,7 @@ def list_secrets(key_vault_uri: str) -> pd.DataFrame:
     """
     Lists all secrets in the specified Azure Key Vault.
 
-    This is a wrapper function for the following API: `List Secrets - List Secrets <https://learn.microsoft.com/rest/api/keyvault/secrets/get-secrets/get-secrets>`_.
+    This is a wrapper function for the following API: `Get Secrets - Get Secrets <https://learn.microsoft.com/rest/api/keyvault/secrets/get-secrets/get-secrets>`_.
 
     Parameters
     ----------
@@ -82,8 +83,30 @@ def list_secrets(key_vault_uri: str) -> pd.DataFrame:
         A pandas DataFrame containing details of all secrets in the Key Vault.
     """
 
-    response = _base_api(request=f"{key_vault_uri}/secrets", client="keyvault")
-    df = pd.json_normalize(response.json().get("value"))
+    columns = {
+        "Secret Id": "str",
+        "Enabled": "bool",
+        "Created Date": "datetime",
+        "Updated Date": "datetime",
+        "Recovery Level": "str",
+        "Recoverable Days": "int",
+    }
+
+    df = _create_dataframe(columns=columns)
+
+    all_items = []
+    url = f"{key_vault_uri}/secrets"
+    while url:
+        response = _base_api(request=url, client="keyvault")
+        result = response.json()
+        items = result.get("value", [])
+        all_items.extend(items)
+        url = result.get("nextLink")  # Update to next page if it exists
+
+    if not all_items:
+        return df
+
+    df = pd.json_normalize(all_items)
     df.rename(
         columns={
             "id": "Secret Id",
@@ -97,6 +120,73 @@ def list_secrets(key_vault_uri: str) -> pd.DataFrame:
     )
     df["Created Date"] = pd.to_datetime(df["Created Date"], unit="s")
     df["Updated Date"] = pd.to_datetime(df["Updated Date"], unit="s")
+    return df
+
+
+@log
+def list_deleted_secrets(key_vault_uri: str) -> pd.DataFrame:
+    """
+    Lists all deleted secrets in the specified Azure Key Vault.
+
+    This is a wrapper function for the following API: `Get Deleted Secrets - GetDeleted Secrets <https://learn.microsoft.com/rest/api/keyvault/secrets/get-deleted-secrets/get-deleted-secrets>`_.
+
+    Parameters
+    ----------
+    key_vault_uri : str
+        Azure Key Vault URI.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A pandas DataFrame containing details of all secrets in the Key Vault.
+    """
+
+    columns = {
+        "Recovery Id": "str",
+        "Deleted Date": "datetime",
+        "Scheduled Purge Date": "datetime",
+        "Content Type": "str",
+        "Secret Id": "str",
+        "Enabled": "bool",
+        "Created Date": "datetime",
+        "Updated Date": "datetime",
+        "Recovery Level": "str",
+    }
+
+    df = _create_dataframe(columns=columns)
+
+    all_items = []
+    url = f"{key_vault_uri}/deletedSecrets"
+    while url:
+        response = _base_api(request=url, client="keyvault")
+        result = response.json()
+        items = result.get("value", [])
+        all_items.extend(items)
+        url = result.get("nextLink")  # Update to next page if it exists
+
+    if not all_items:
+        return df
+
+    df = pd.json_normalize(all_items)
+    df.rename(
+        columns={
+            "recoveryId": "Recovery Id",
+            "deletedDate": "Deleted Date",
+            "scheduledPurgeDate": "Scheduled Purge Date",
+            "contentType": "Content Type",
+            "id": "Secret Id",
+            "attributes.enabled": "Enabled",
+            "attributes.created": "Created Date",
+            "attributes.updated": "Updated Date",
+            "attributes.recoveryLevel": "Recovery Level",
+        },
+        inplace=True,
+    )
+    df["Scheduled Purge Date"] = pd.to_datetime(df["Scheduled Purge Date"], unit="s")
+    df["Deleted Date"] = pd.to_datetime(df["Deleted Date"], unit="s")
+    df["Created Date"] = pd.to_datetime(df["Created Date"], unit="s")
+    df["Updated Date"] = pd.to_datetime(df["Updated Date"], unit="s")
+
     return df
 
 
