f7c4fe000eb4804e5040e3e3d941a7d4356a7ac5
Michael <mikova@microsoft.com>
2025-06-18 21:17:10 +0300

    org themes

---
+++ src/sempy_labs/theme/_org_themes.py
@@ -0,0 +1,96 @@
+import pandas as pd
+from uuid import UUID
+from sempy_labs._helper_functions import (
+    _is_valid_uuid,
+    _base_api,
+    _update_dataframe_datatypes,
+    _create_dataframe,
+)
+
+
+_pbi_url_prefix = None
+
+
+def init_pbi_url_prefix():
+    global _pbi_url_prefix
+    if _pbi_url_prefix:
+        return
+
+    response = _base_api("/v1.0/myorg/capacities")
+    _pbi_url_prefix = response.json().get("@odata.context").split("/v1.0/myorg")[0]
+
+
+def list_org_themes() -> pd.DataFrame:
+
+    init_pbi_url_prefix()
+
+    columns = {
+        "Theme Id": "str",
+        "Theme Name": "str",
+        "Description": "str",
+        "Enabled In Theme Gallery": "bool",
+        "Update Time": "datetime",
+        "For Copilot": "bool",
+        "Theme Json": "str",
+    }
+
+    df = _create_dataframe(columns=columns)
+
+    response = _base_api(request=f"{_pbi_url_prefix}/metadata/organization/themes")
+
+    result = response.json().get("orgThemes")
+    if result:
+        df = pd.json_normalize(result)
+        df.rename(
+            columns={
+                "objectId": "Theme Id",
+                "displayName": "Theme Name",
+                "description": "Description",
+                "enabledInThemeGallery": "Enabled In Theme Gallery",
+                "updateTime": "Update Time",
+                "forCopilot": "For Copilot",
+                "themeJson": "Theme Json",
+            },
+            inplace=True,
+        )
+        _update_dataframe_datatypes(df, columns)
+
+    return df
+
+
+def resolve_theme_id(theme: str | UUID):
+
+    if _is_valid_uuid(theme):
+        return theme
+    else:
+        df = list_org_themes()
+        df_filt = df[df["Theme Name"] == theme]
+        if df_filt.empty:
+            raise ValueError("Invalid theme")
+        else:
+            return df_filt["Theme Id"].iloc[0]
+
+
+def get_org_theme(theme: str | UUID) -> dict:
+
+    init_pbi_url_prefix()
+
+    theme_id = resolve_theme_id(theme)
+    response = _base_api(
+        request=f"{_pbi_url_prefix}/metadata/organization/themes/{theme_id}"
+    )
+    return response.json().get("themeJson", {})
+
+
+def delete_org_theme(theme: str | UUID) -> None:
+    """
+    Deletes an organization theme by its name or ID.
+    """
+    init_pbi_url_prefix()
+
+    theme_id = resolve_theme_id(theme)
+    _base_api(
+        request=f"{_pbi_url_prefix}/metadata/organization/themes/{theme_id}",
+        method="delete",
+        status_codes=204,
+    )
