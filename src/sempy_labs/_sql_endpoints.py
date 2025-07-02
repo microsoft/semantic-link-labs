e8f6e0da7779ea2a9e3d9ee88b470c0133ee33aa
Erlend Øien <erlend.oeien@gmail.com>
2025-07-02 08:43:23 +0200

    whitespace

--- src/sempy_labs/_sql_endpoints.py
+++ src/sempy_labs/_sql_endpoints.py
@@ -1,16 +1,15 @@
-from typing import Optional, Literal
+from typing import Literal, Optional
 from uuid import UUID
+
 import pandas as pd
-from sempy_labs._helper_functions import (
-    _base_api,
-    _create_dataframe,
-    resolve_workspace_name_and_id,
-    resolve_item_name_and_id,
-    _update_dataframe_datatypes,
-    resolve_workspace_id,
-)
-import sempy_labs._icons as icons
 from sempy._utils._log import log
+
+import sempy_labs._icons as icons
+from sempy_labs._helper_functions import (_base_api, _create_dataframe,
+                                          _update_dataframe_datatypes,
+                                          resolve_item_name_and_id,
+                                          resolve_workspace_id,
+                                          resolve_workspace_name_and_id)
 
 
 @log
