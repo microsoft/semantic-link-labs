9cce0cd50a3bb31adbd59af97e22d4d6b9d6c1d7
Michael <mikova@microsoft.com>
2025-09-14 11:00:55 +0300

    bind semantic model

--- src/sempy_labs/__init__.py
+++ src/sempy_labs/__init__.py
@@ -31,6 +31,8 @@ from ._semantic_models import (
     delete_semantic_model,
     update_semantic_model_refresh_schedule,
     list_semantic_model_datasources,
+    bind_semantic_model_connection,
+    unbind_semantic_model_connection,
 )
 from ._graphQL import (
     list_graphql_apis,
@@ -605,4 +607,6 @@ __all__ = [
     "set_workspace_network_communication_policy",
     "get_warehouse_connection_string",
     "list_data_access_roles",
+    "bind_semantic_model_connection",
+    "unbind_semantic_model_connection",
 ]
