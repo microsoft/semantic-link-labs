f1e2ff1e29fbb531d378ba7a617d220954ddda94
Michael <mikova@microsoft.com>
2026-01-05 09:14:54 +0200

    added docs

--- src/sempy_labs/_takeover.py
+++ src/sempy_labs/_takeover.py
@@ -15,6 +15,20 @@ def takeover_item_ownership(
     type: Literal["Report", "SemanticModel"],
     workspace: Optional[str | UUID] = None,
 ):
+    """
+    Takes over ownership of a Report or Semantic Model.
+
+    Parameters
+    ----------
+    item : str | uuid.UUID
+        The name or UUID of the Report or Semantic Model to take over.
+    type : Literal["Report", "SemanticModel"]
+        The type of item to take over. Must be either 'Report' or 'SemanticModel'.
+    workspace : Optional[str | uuid.UUID], default=None
+        The name or ID of the workspace.
+        Defaults to None which resolves to the workspace of the attached lakehouse
+        or if no lakehouse attached, resolves to the workspace of the notebook.
+    """
 
     if type not in ["Report", "SemanticModel"]:
         raise ValueError(
