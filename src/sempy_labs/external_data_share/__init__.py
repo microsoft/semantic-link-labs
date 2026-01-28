53c923233bea8bb760b459a59ec2708e13477146
Michael <mikova@microsoft.com>
2026-01-28 19:56:28 +0200

    external data share package

---
+++ src/sempy_labs/external_data_share/__init__.py
@@ -0,0 +1,13 @@
+from ._items import (
+    create_external_data_share,
+    delete_external_data_share,
+    list_external_data_shares_in_item,
+    revoke_external_data_share,
+)
+
+__all__ = [
+    "create_external_data_share",
+    "delete_external_data_share",
+    "list_external_data_shares_in_item",
+    "revoke_external_data_share",
+]
