6a3721d9358d01c57e7a5585785ed755b3f61323
Michael <mikova@microsoft.com>
2026-03-18 11:35:20 +0200

    0.14.0

--- src/sempy_labs/_a_lib_info.py
+++ src/sempy_labs/_a_lib_info.py
@@ -4,7 +4,7 @@ import zipfile
 from pathlib import Path
 
 lib_name = "semanticlinklabs"
-lib_version = "0.13.2"
+lib_version = "0.14.0"
 
 NUGET_BASE_URL = "https://www.nuget.org/api/v2/package"
 current_dir = Path(__file__).parent
