94fa989e3c5a51a7d5ff72f4f1040280c30608b9
m-kovalsky <mikova@microsoft.com>
2026-05-19 09:50:35 +0300

    onelake lifecycle policy

---
+++ src/sempy_labs/onelake/__init__.py
@@ -0,0 +1,11 @@
+from ._lifecycle_policy import (
+    export_onelake_lifecycle_policy,
+    import_onelake_lifecycle_policy,
+    delete_onelake_lifecycle_policy,
+)
+
+__all__ = [
+    "export_onelake_lifecycle_policy",
+    "import_onelake_lifecycle_policy",
+    "delete_onelake_lifecycle_policy",
+]
