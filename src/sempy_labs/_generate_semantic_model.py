aeac029be52ac798ad5a4e347a86da0d30332595
Michael Kovalsky <mikova@microsoft.com>
2026-07-19 09:24:40 +0300

    1284

--- src/sempy_labs/_generate_semantic_model.py
+++ src/sempy_labs/_generate_semantic_model.py
@@ -614,7 +614,7 @@ def get_semantic_model_definition(
 @log
 def get_semantic_model_size(
     dataset: str | UUID, workspace: Optional[str | UUID] = None
-):
+) -> int:
     """
     Gets size of the semantic model in bytes.
 
@@ -630,35 +630,26 @@ def get_semantic_model_size(
     Returns
     -------
     int
-        The size of the semantic model in bytes
+        The size of the semantic model in bytes.
     """
+    dax = """
+    EVALUATE
+    ROW(
+        "TotalSize",
+            SUMX(
+                INFO.STORAGETABLECOLUMNSEGMENTS(),
+                [USED_SIZE]
+            )
+        +
+            SUMX(
+                FILTER(
+                    INFO.STORAGETABLECOLUMNS(),
+                    [COLUMN_TYPE] = "BASIC_DATA"
+                ),
+                [DICTIONARY_SIZE]
+            )
+    )"""
 
-    dict = fabric.evaluate_dax(
-        dataset=dataset,
-        workspace=workspace,
-        dax_string="""
-        EVALUATE SELECTCOLUMNS(FILTER(INFO.STORAGETABLECOLUMNS(), [COLUMN_TYPE] = "BASIC_DATA"),[DICTIONARY_SIZE])
-        """,
-    )
+    result = fabric.evaluate_dax(dataset=dataset, workspace=workspace, dax_string=dax)
 
-    used_size = fabric.evaluate_dax(
-        dataset=dataset,
-        workspace=workspace,
-        dax_string="""
-        EVALUATE SELECTCOLUMNS(INFO.STORAGETABLECOLUMNSEGMENTS(),[USED_SIZE])
-        """,
-    )
-    dict_size = dict["[DICTIONARY_SIZE]"].sum()
-    used_size = used_size["[USED_SIZE]"].sum()
-    model_size = dict_size + used_size
-    # Calculate proper bytes size by dividing by 1024 and multiplying by 1000 - per 1000
-    if model_size >= 10**9:
-        result = model_size / (1024**3) * 10**9
-    elif model_size >= 10**6:
-        result = model_size / (1024**2) * 10**6
-    elif model_size >= 10**3:
-        result = model_size / (1024) * 10**3
-    else:
-        result = model_size
-
-    return result
+    return int(float(result.iloc[0, 0]))
